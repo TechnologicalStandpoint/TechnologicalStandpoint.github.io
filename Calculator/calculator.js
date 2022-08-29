ac3a99d42219f2fa7ecd728cae5d0b0b2fe0fb2d
William Kennedy <wkenney@protonmail.com>
2022-08-29 10:35:01 -0400

    Completed infix to postfix conversion

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -5,8 +5,12 @@
 const _output = document.getElementById('output');
 const _clear = document.getElementById('clear');
 const _equal = document.getElementById('equals');
-
 const buttonInputs = document.getElementsByClassName("input");
+
+// Map defining the precedence of the operators
+const _operators = {'^': 3, '/': 2, 'x': 2, '+': 1, '-': 1};
+const opStack = [];
+let output = [];
 
 // Assigns an anymous function to each input button on the calculator
 for (let i=0; i < buttonInputs.length; i++) {
@@ -33,16 +37,15 @@ _clear.onclick = function() {
 _equal.onclick = function() {
   const formula = _output.innerHTML;
   const arrFormula = formula.split("");
-  const opStack = [];
-  const output = [];
+  output = [];
 
-  // Puts 
-
+  // Converts from infix to postfix form
   arrFormula.forEach(char => {
+
     switch(char) {
       case '(': 
         opStack.push(char);
-      
+        break;
       case ')':
         // Case calculates operations of the current parethenese's
         i = opStack.pop();
@@ -50,28 +53,60 @@ _equal.onclick = function() {
           output.push(i)
           i = opStack.pop();
         }
+        break;
 
       case '^':
-        opStack.push(char);
+        removeOperator(char);
+        break;
         
       case '+':
-        opStack.push(char);
-      
-      case '-': 
-        opStack.push(char);
+        removeOperator(char);
+        break;
 
-      case '*':
-        opStack.push(char);
+      case '-': 
+        removeOperator(char);
+        break;
+
+      case 'x':
+        removeOperator(char);
+        break;
 
       case '/':
-        opStack.push(char);
-      
+        removeOperator(char);
+        break;
+
       default:
+        output.push(char);
         break;
     }
   });
+  
+  while (opStack.length !== 0) {
+    output.push(opStack.pop());
+  }
+  _output.innerHTML = output.join('');
 }
 
-function removePrecednce(char) {
+// Helper function that solves the two cases of adding an operator to the opStack
+// 1) If the top element has the same or lower precedence, it pushes the new operator to the opStack
+// 2) 
+function removeOperator(op) {
 
+  let top = opStack[opStack.length-1];
+  if (opStack.length === 0) {
+
+    opStack.push(op);
+
+  } else {
+
+    while (opStack.length !== 0) {
+
+      if (_operators[top] > _operators[op]) {
+        output.push(opStack.pop());
+      } else {
+        break;
+      }
+    }
+    opStack.push(op);
+  }
 }
